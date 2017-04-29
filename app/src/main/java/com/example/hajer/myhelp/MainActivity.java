bdff0b356c04ee2eb51e11869a7d9ac309912940
hk476@drexel.edu <hk476@drexel.edu>
2017-04-29 19:38:56 -0400

    changes

--- app/src/main/java/com/example/hajer/myhelp/MainActivity.java
+++ app/src/main/java/com/example/hajer/myhelp/MainActivity.java
@@ -1,4 +1,14 @@
-package com.example.iaman.myapplication;
+package com.example.hajer.myhelp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
