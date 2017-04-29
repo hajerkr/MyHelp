4774832209ae99889e60a679b2510dc72f49649a
LAPTOP-QF0U1UHN\iaman <ay372@drexel.edu>
2017-04-29 19:34:36 -0400

    Java
    Code for button sound

--- app/src/main/java/com/example/hajer/myhelp/MainActivity.java
+++ app/src/main/java/com/example/hajer/myhelp/MainActivity.java
@@ -1,64 +1,28 @@
-package com.example.hajer.myhelp;
+package com.example.iaman.myapplication;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.media.MediaPlayer;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton foodpic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        foodpic = (ImageButton) findViewById(R.id.food);
-        foodpic.setOnClickListener(new View.OnClickListener() {
-           // public void onClick(View v) {
 
-                //do stuff
-            }
-
-        }
-        }
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button btn1 = (Button) this.findViewById(R.id.btnPlaySound1);
+        final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.soho);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                mp1.start();
+
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
