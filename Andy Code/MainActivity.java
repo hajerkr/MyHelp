2b5c2d61bb7be21df4c116743b1f5569ae3c2563
LAPTOP-QF0U1UHN\iaman <ay372@drexel.edu>
2017-04-29 19:24:34 -0400

    code
    Andy Code for sounds

---
+++ Andy Code/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.iaman.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.media.MediaPlayer;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btn1 = (Button) this.findViewById(R.id.btnPlaySound1);
+        final MediaPlayer mp1 = MediaPlayer.create(this, R.raw.soho);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp1.start();
+
+            }
+        });
+    }
+}
